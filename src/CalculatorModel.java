b3eb17f3c61149b055723965e1cf31c2b28fd4eb
silence0925 <h55334147879@gmail.com>
2019-10-04 15:26:34 +0800

    Model update

--- src/CalculatorModel.java
+++ src/CalculatorModel.java
@@ -2,13 +2,11 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class CalculatorModel {
+	
+	
 	Stack<String> expression;
 	
 	public CalculatorModel(){
-		initialize();
-	}
-	
-	private void initialize() {
 		expression = new Stack<String>();
 	}
 	
@@ -16,13 +14,6 @@ public class CalculatorModel {
 		return expression.isEmpty();
 	}
 	
-	public void clear() {
-		expression.clear();
-	}
-	
-	public void set(String element, int index) {
-		expression.set(index, element);
-	}
 	
 	public void push(String element) {
 		expression.add(element);
@@ -32,70 +23,140 @@ public class CalculatorModel {
 		return expression.pop();
 	}
 	
-	public String top() {
-		return expression.peek();
+	public void clear() {
+		expression.clear();
 	}
 	
+	
 	public String getInfix() {
+		
 		String res = "";
-		for(String op : expression) {
-			res += op.contains(CalUtils.minusStr) ? "(" + op + ")" : op;
+		String op = "" ;
+		
+		for(int i = 0 ; i < expression.size(); i++) {
+		
+			op = expression.get(i) ;
+			//如果有負數 給他一個可愛的括號
+			if(op.contains(CalUtils.minusStr))
+				res = res + "(" + op + ")" ;
+			else
+				res += op ;	
+		
 		}
 		return res;
 	}
 	
+	
+	private static int priority(String operator) {
+		
+		if( operator.equals(CalUtils.addStr) || operator.equals(CalUtils.subStr) )
+			return 0 ; // + -
+		
+		else
+			return 1 ; // * 或 除法
+		
+		
+		
+	}
+	
+	
 	private ArrayList<String> getPostfix(){
-		System.out.println(expression);
-		Stack<String> tmp = new Stack<String>();
+		
 		ArrayList<String> postfix = new ArrayList<String>();
-		for(int i = 0; i < expression.size(); ++i) {
+		Stack<String> temp = new Stack<String>();
+		
+		
+		
+		
+		for(int i = 0; i < expression.size(); i++) {
+			
 			if(i % 2 == 0) {
 				postfix.add(expression.get(i));
-			} else {
+			} 
+			
+			else {
+				
 				String operator = expression.get(i);
-				while(!tmp.isEmpty() && priority(tmp.peek()) >= priority(operator)) {
-					postfix.add(tmp.pop());
+				
+				while(!temp.isEmpty() && priority(temp.peek()) >= priority(operator)) 
+				{
+					postfix.add(temp.pop());
 				}
-				tmp.add(operator);
+				temp.add(operator);
 			}
 		}
-		while(!tmp.isEmpty()) {
-			postfix.add(tmp.pop());
+		
+		
+		while(!temp.isEmpty()) 
+		{
+			postfix.add(temp.pop());
 		}
+		
+		System.out.println("前序:");
+		System.out.println(expression);
+		System.out.println("後序:");
 		System.out.println(postfix);
 		return postfix;
+		
 	}
 	
-	public String calculate() {
+	public String calculate() 
+	{
 		ArrayList<String> postfix = getPostfix();
-		Stack<Float> values = new Stack<Float>();
-		for(String element : postfix) {
+		Stack<Float> num = new Stack<Float>();
+		String element = "" ;
+		
+		for(int i = 0 ; i < postfix.size(); i++) {
+			
+			element = postfix.get(i) ;
+			
 			if(CalUtils.isOperator(element)) {
-				float y = values.pop();
-				float x = values.pop();
-				values.push(evaluate(x, y, element));
-			} else {
-				values.push(Float.valueOf(element));
+				
+				float y = num.pop();
+				
+				float x = num.pop();
+				
+				num.push(cal(x, y, element));
+			} 
+			
+			else {
+				
+				num.push(Float.valueOf(element));
 			}
 		}
-		return CalUtils.trimPointZero(String.valueOf(values.peek()));
+		
+		return CalUtils.trimPointZero(String.valueOf(num.peek()));
+	
 	}
 	
 	
-	private static float evaluate(float x, float y, String operator) {
-		if(operator.equals(CalUtils.addStr)) {
-			return x + y;
-		} else if(operator.equals(CalUtils.subStr)) {
-			return x - y;
-		} else if(operator.equals(CalUtils.mulStr)) {
-			return x * y;
-		} else if(operator.equals(CalUtils.divStr)) {
-			return x / y;
+	private static float cal(float a, float b, String operator) {
+		if(operator.equals(CalUtils.addStr)) 
+		{
+			
+			return a + b;
+			
+		} 
+		else if(operator.equals(CalUtils.subStr)) 
+		{
+			
+			return a - b;
+			
+		} 
+		else if(operator.equals(CalUtils.divStr)) 
+		{
+			return a / b;
 		}
+		
+		else if(operator.equals(CalUtils.mulStr)) 
+		{
+			return a * b;
+		} 
+		
+		
 		return 0;
 	}
 	
-	private static int priority(String operator) {
-		return operator.equals(CalUtils.addStr) || operator.equals(CalUtils.subStr) ? 0 : 1;
-	}
+	
+	
 }
