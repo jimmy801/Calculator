04517f8686725f0552db3209cf9762123dcb4d96
jimmy801 <jimmycf4gg@gmail.com>
2019-10-15 16:31:05 +0800

    add comment of CalculatorModel.java

--- src/CalculatorModel.java
+++ src/CalculatorModel.java
@@ -1,41 +1,80 @@
 import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * 
+ * model of calculator
+ * 
+ * @author Jimmy801
+ *
+ */
 public class CalculatorModel {
-	Stack<String> expression;
 
+	/** save all digits & operators */
+	private Stack<String> expression;
+
+	/**
+	 * objector constructor
+	 */
 	public CalculatorModel() {
 		initialize();
 	}
 
+	/**
+	 * initialize function
+	 */
 	private void initialize() {
 		expression = new Stack<String>();
 	}
 
+	/**
+	 * check expression is empty or not
+	 * 
+	 * @return True/False
+	 */
 	public boolean isEmpty() {
 		return expression.isEmpty();
 	}
 
+	/**
+	 * clear all digits & operators in expression
+	 */
 	public void clear() {
 		expression.clear();
 	}
 
-	public void set(String element, int index) {
-		expression.set(index, element);
-	}
-
+	/**
+	 * put an element to expression
+	 * 
+	 * @param element - {@link String} digit or operator
+	 */
 	public void push(String element) {
 		expression.add(element);
 	}
 
+	/**
+	 * return top of expression and remove it form expression
+	 * 
+	 * @return {@link String} top of expression
+	 */
 	public String pop() {
 		return expression.pop();
 	}
 
+	/**
+	 * return top of expression
+	 * 
+	 * @return {@link String} top of expression
+	 */
 	public String top() {
 		return expression.peek();
 	}
 
+	/**
+	 * get infix of expression
+	 * 
+	 * @return {@link String} infix of expression
+	 */
 	public String getInfix() {
 		StringBuilder res = new StringBuilder();
 		for (String op : expression) {
@@ -44,6 +83,12 @@ public class CalculatorModel {
 		return res.toString();
 	}
 
+	/**
+	 * get postfix of specific stack
+	 * 
+	 * @param expre - {@link Stack}<{@link String}>specific stack of expression
+	 * @return {@link String} postfix of specific stack
+	 */
 	private ArrayList<String> getPostfix(Stack<String> expre) {
 		System.out.println(expre);
 		Stack<String> tmp = new Stack<String>();
@@ -66,6 +111,12 @@ public class CalculatorModel {
 		return postfix;
 	}
 
+	/**
+	 * calculate the answer of specific stack
+	 * 
+	 * @param expre - {@link Stack}<{@link String}>specific stack of expression
+	 * @return {@link String} the answer of specific stack
+	 */
 	public String calculate(Stack<String> expre) {
 		ArrayList<String> postfix = getPostfix(expre);
 		Stack<Float> values = new Stack<Float>();
@@ -81,10 +132,22 @@ public class CalculatorModel {
 		return CalUtils.trimPointZero(String.valueOf(values.peek()));
 	}
 
+	/**
+	 * calculate the answer of expression
+	 * 
+	 * @return {@link String} the answer of expression
+	 */
 	public String calculate() {
 		return calculate(expression);
 	}
 
+	/**
+	 * parse a formula string, and split the numbers and operators
+	 * 
+	 * @param exp - formula string
+	 * @return {@link ArrayList}<{@link String}> all numbers and operators of
+	 *         formula string
+	 */
 	public ArrayList<String> parseAndSplitString(String exp) {
 		ArrayList<String> strs = new ArrayList<>();
 		String num = "";
@@ -110,12 +173,23 @@ public class CalculatorModel {
 		return strs;
 	}
 
+	/**
+	 * add formula to current expression
+	 * 
+	 * @param input - {@link String} parsed formula string
+	 */
 	public void parse(String input) {
 		String exp = input.replaceAll("[\\s\\u3000\t,]", "");
 		ArrayList<String> parseStr = parseAndSplitString(exp);
 		expression.addAll(parseStr);
 	}
 
+	/**
+	 * check the input string is valid formula string or not
+	 * 
+	 * @param input - {@link String} formula string
+	 * @return True/False
+	 */
 	public boolean tryParse(String input) {
 		String exp = input.replaceAll("[\\s\\u3000\t,]", "");
 		String regex = String.format("[\\d%s]", "\\" + CalUtils.addStr + CalUtils.subStr + "\\" + CalUtils.minusStr
@@ -123,7 +197,7 @@ public class CalculatorModel {
 		if (!exp.replaceAll(regex, "").isEmpty())
 			return false;
 
-		Stack<String> stk = new Stack();
+		Stack<String> stk = new Stack<String>();
 		ArrayList<String> parseStr = parseAndSplitString(exp);
 		stk.addAll(expression);
 		if (!stk.isEmpty() && stk.peek().replaceAll("[\\d\\.]", "").isEmpty()
@@ -143,6 +217,14 @@ public class CalculatorModel {
 		return true;
 	}
 
+	/**
+	 * calculate the result of x operator y
+	 * 
+	 * @param x        - number1
+	 * @param y        - number2
+	 * @param operator - operator of 2 parameters
+	 * @return the result of x operator y
+	 */
 	private static float evaluate(float x, float y, String operator) {
 		if (operator.equals(CalUtils.addStr)) {
 			return x + y;
@@ -156,6 +238,12 @@ public class CalculatorModel {
 		return 0;
 	}
 
+	/**
+	 * return priority of operator
+	 * 
+	 * @param operator - {@link String}the operator
+	 * @return 0 if operator is addition or subtraction, other is 1
+	 */
 	private static int priority(String operator) {
 		return operator.equals(CalUtils.addStr) || operator.equals(CalUtils.subStr) ? 0 : 1;
 	}
